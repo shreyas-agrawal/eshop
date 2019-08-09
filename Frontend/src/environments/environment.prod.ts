196c03b43e603988b67527f528e1d5b1e2313b46
shreyas-agrawal <shreyasagrawal12345@gmail.com>
2019-08-09 22:55:29 +0530

    created some basic components for application

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: "AIzaSyDjT1hvxprC69FE8sFNVm22Bs3uvwEv3V8",
+    authDomain: "eshop-16ed8.firebaseapp.com",
+    databaseURL: "https://eshop-16ed8.firebaseio.com",
+    projectId: "eshop-16ed8",
+    storageBucket: "eshop-16ed8.appspot.com",
+    messagingSenderId: "815099025466",
+    appId: "1:815099025466:web:0d7b8c70161c4aad"
+  }
 };
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyDjT1hvxprC69FE8sFNVm22Bs3uvwEv3V8",
+    authDomain: "eshop-16ed8.firebaseapp.com",
+    databaseURL: "https://eshop-16ed8.firebaseio.com",
+    projectId: "eshop-16ed8",
+    storageBucket: "eshop-16ed8.appspot.com",
+    messagingSenderId: "815099025466",
+    appId: "1:815099025466:web:0d7b8c70161c4aad"
+  }
 };
 
 /*
