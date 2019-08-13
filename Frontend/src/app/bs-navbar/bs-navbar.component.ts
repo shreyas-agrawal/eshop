acb2e923b160a8dd37663344503d706289bfe54a
shreyas-agrawal <shreyasagrawal12345@gmail.com>
2019-08-13 21:32:41 +0530

    extracted navigation bar in seprate component

--- Frontend/src/app/bs-navbar/bs-navbar.component.ts
+++ Frontend/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-bs-navbar',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BsNavbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(public auth: AuthService) {}
 
   ngOnInit() {
+  }
+
+  logout(){
+    this.auth.logout();
   }
 
 }
