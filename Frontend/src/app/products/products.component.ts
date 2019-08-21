d81e4312a905afb7bbb3f3ab1d0fbef3f8292b7f
shreyas-agrawal <shreyasagrawal12345@gmail.com>
2019-08-21 23:37:00 +0530

    added home page menu to view items for user

--- Frontend/src/app/products/products.component.ts
+++ Frontend/src/app/products/products.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-products',
@@ -7,7 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
+  products = [];
+  categories;
+  product = {};
+
+  constructor(
+    private productService: ProductService,
+    private categoryService: CategoryService) {
+    this.productService.getAllProducts().snapshotChanges().subscribe((products) => {
+      products.forEach(p => {
+        this.product = p.payload.val();
+        this.product['key'] = p.key;
+        this.products.push(this.product);
+      });
+    });
+    this.categoryService.getCategories().snapshotChanges().subscribe((categories) => {
+      this.categories = categories;
+      console.log(this.categories);
+    });
+  }
 
   ngOnInit() {
   }
