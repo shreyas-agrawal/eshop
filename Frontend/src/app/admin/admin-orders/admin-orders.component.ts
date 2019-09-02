fd7c2dbff20f0d5c9e830c2af9bf653cf05d9f54
shreyas-agrawal <shreyasagrawal12345@gmail.com>
2019-09-02 23:45:18 +0530

    Added code to view orders for the user and admin

--- Frontend/src/app/admin/admin-orders/admin-orders.component.ts
+++ Frontend/src/app/admin/admin-orders/admin-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from 'src/app/services/order.service';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-admin-orders',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminOrdersComponent implements OnInit {
 
-  constructor() { }
+  orders = [];
+  order = {};
+  user$;
+  username: string;
+
+  constructor(
+    private orderService: OrderService,
+    private auth: AuthService) { }
 
   ngOnInit() {
+    this.orderService.getOrders().subscribe(orders => {
+      console.log('orders', orders);
+      orders.forEach(item => {
+        // console.log('order', item);
+        this.order = item.payload.val();
+        this.order['key'] = item.key;
+        this.orders.push(this.order);
+      });
+      console.log(this.orders);
+    });
+  }
+
+  getDate(time){
+    const date = new Date(time);
+    return date.toString().substring(4, 15);
+    // return date.getDate() + ' ' + date.getMonth() + ' ' + date.getFullYear();
   }
 
 }
--- Frontend/src/app/my-orders/my-orders.component.ts
+++ Frontend/src/app/my-orders/my-orders.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { OrderService } from '../services/order.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-my-orders',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyOrdersComponent implements OnInit {
 
-  constructor() { }
+  orders = [];
+  order = {};
+  user$;
+  userId: string;
+
+  constructor(
+    private orderService: OrderService,
+    private auth: AuthService) { }
 
   ngOnInit() {
+    this.auth.user$.subscribe(user => {
+      this.userId = user.uid;
+
+      this.orderService.getOrdersByUser(this.userId).subscribe(orders => {
+        console.log('orders', orders);
+        orders.forEach(item => {
+          // console.log('order', item);
+          this.order = item.payload.val();
+          this.order['key'] = item.key;
+          this.orders.push(this.order);
+        });
+        console.log(this.orders);
+      });
+
+    });
   }
 
+  getDate(time){
+    const date = new Date(time);
+    return date.toString().substring(4, 15);
+    // return date.getDate() + ' ' + date.getMonth() + ' ' + date.getFullYear();
+  }
 }
